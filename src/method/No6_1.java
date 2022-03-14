28e132f33cdebe4b6baae2f1469b856699526b19
unknown <92510176+yooyama130@users.noreply.github.com>
2022-03-14 15:51:16 +0900

    [Finish] メソッド6-1、メソッド6-2

---
+++ src/method/No6_1.java
@@ -0,0 +1,15 @@
+package method;
+
+//—ûK–â‘èêŠF https://kitako.tokyo/lib/JavaExercise.aspx?id=6
+
+public class No6_1 {
+	public static void main(String[] args) {
+		int number = squared(3);
+		System.out.println(number);
+	}
+	
+	//ˆø”‚ğ2æ‚·‚é
+	public static int squared(int num) {
+		return num*num;
+	}
+}
